61cd5cd11060541068d4db9ecc23c537bc4d158d
SurvivorsGames <survivorsgamescol@gmail.com>
2020-10-03 12:41:04 -0500

    final fix

--- src/Components/Forms/ExamenClinico.js
+++ src/Components/Forms/ExamenClinico.js
@@ -42,119 +42,119 @@ export default function ExamenClinico(){
             <div className="mt-5 mx-auto">
                 <h4 className="text-black mb-5">Examen por sistemas</h4>
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6">Ojos:</strong>
-                    <input className="form-control col-sm-6 " placeholder="status" id="in1"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6  mx-right" placeholder="Hallazgos anormales" id="in2"/>
+                    <strong className="text-black col-sm-6 col-md-5">Ojos:</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status" id="in1"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7  mx-right" placeholder="Hallazgos anormales" id="in2"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6 ">Oidos:</strong>
-                    <input className="form-control col-sm-6 " placeholder="status" id="in1"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales" id="in2"/>
+                    <strong className="text-black col-sm-6 col-md-5 ">Oidos:</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status" id="in1"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales" id="in2"/>
                 </div> 
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Respiratorio</strong>
-                    <input className="form-control col-sm-6 " placeholder="status" id="in1"/>
-                    <label className="text-black col-sm-6 ">Nariz:</label>
-                    <input className="form-control col-sm-6 " placeholder="Nariz" id="in2"/>
-                    <label className="text-black col-sm-6 ">Patron Respiratorio:</label>
-                    <input className="form-control col-sm-6 " placeholder="Patron Respiratorio" id="in3"/>
-                    <label className="text-black col-sm-6 ">Campo Pulmonar:</label>
-                    <input className="form-control col-sm-6 " placeholder="Campo Pulmonar" id="in4"/>
-                    <label className="text-black col-sm-6 ">Signos:</label>
-                    <input className="form-control col-sm-6 " placeholder="Signos" id="in5"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Respiratorio</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status" id="in1"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Nariz:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Nariz" id="in2"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Patron Respiratorio:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Patron Respiratorio" id="in3"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Campo Pulmonar:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Campo Pulmonar" id="in4"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Signos:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Signos" id="in5"/>
                 </div> 
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Digestivo</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Cavidad oral:</label>
-                    <input className="form-control col-sm-6 " placeholder="Nariz"/>
-                    <label className="text-black col-sm-6 ">Apetito:</label>
-                    <input className="form-control col-sm-6 " placeholder="Patron Respiratorio"/>
-                    <label className="text-black col-sm-6 ">Consumo de agua:</label>
-                    <input className="form-control col-sm-6 " placeholder="Campo Pulmonar"/>
-                    <label className="text-black col-sm-6 ">Defecacion:</label>
-                    <input className="form-control col-sm-6 " placeholder="Defecacion"/>
-                    <label className="text-black col-sm-6 ">Signos:</label>
-                    <input className="form-control col-sm-6 " placeholder="Signos"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Digestivo</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Cavidad oral:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Nariz"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Apetito:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Patron Respiratorio"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Consumo de agua:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Campo Pulmonar"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Defecacion:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Defecacion"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Signos:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Signos"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Tegumentario</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Glandulas mamarias:</label>
-                    <input className="form-control col-sm-6 " placeholder="Glandulas mamarias"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales"/>
-                    <label className="text-black col-sm-6 ">Ectoparasitos:</label>
-                    <input className="form-control col-sm-6 " placeholder="Extoparasitos"/>
-                    <label className="text-black col-sm-6 ">Prurito:</label>
-                    <input className="form-control col-sm-6 " placeholder="Prurito"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Tegumentario</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Glandulas mamarias:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Glandulas mamarias"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Ectoparasitos:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Extoparasitos"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Prurito:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Prurito"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Cardiovascular</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Cardiovascular</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Muscoloesqueletico</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Claudicacion:</label>
-                    <input className="form-control col-sm-6 " placeholder="Claudicacion"/>
-                    <label className="text-black col-sm-6 ">Masa muscular:</label>
-                    <input className="form-control col-sm-6 " placeholder="Masa muscular"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Muscoloesqueletico</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Claudicacion:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Claudicacion"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Masa muscular:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Masa muscular"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Nervioso</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Nivel de consciencia:</label>
-                    <input className="form-control col-sm-6 " placeholder="Nivel de consciencia"/>
-                    <label className="text-black col-sm-6 ">Actitud y postura:</label>
-                    <input className="form-control col-sm-6 " placeholder="Actitud y postura"/>
-                    <label className="text-black col-sm-6 ">Marcha:</label>
-                    <input className="form-control col-sm-6 " placeholder="Marcha"/>
-                    <label className="text-black col-sm-6 ">Acomodacion propioceptiva:</label>
-                    <input className="form-control col-sm-6 " placeholder="Acomodacion"/>
-                    <label className="text-black col-sm-6 ">Dolor:</label>
-                    <input className="form-control col-sm-6 " placeholder="Dolor"/>
-                    <label className="text-black col-sm-6 ">Tono muscular:</label>
-                    <input className="form-control col-sm-6 " placeholder="Tono muscular"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Nervioso</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Nivel de consciencia:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Nivel de consciencia"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Actitud y postura:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Actitud y postura"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Marcha:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Marcha"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Acomodacion propioceptiva:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Acomodacion"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Dolor:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Dolor"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Tono muscular:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Tono muscular"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Urinario</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Miccion:</label>
-                    <input className="form-control col-sm-6 " placeholder="Claudicacion"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Urinario</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Miccion:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Claudicacion"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales"/>
                 </div>
 
                 <div className="form-group row w-100 mx-auto">
-                    <strong className="text-black col-sm-6  strong">Sistema Reproductivo</strong>
-                    <input className="form-control col-sm-6 " placeholder="status"/>
-                    <label className="text-black col-sm-6 ">Vulva/Pene:</label>
-                    <input className="form-control col-sm-6 " placeholder="Vulva/pene"/>
-                    <label className="text-black col-sm-6 ">Escroto y testiculos:</label>
-                    <input className="form-control col-sm-6 " placeholder="Escroto y testiculos"/>
-                    <label className="text-black col-sm-6 ">Ultimo celo:</label>
-                    <input className="form-control col-sm-6 " placeholder="Ultimo celo"/>
-                    <label className="text-black col-sm-6 ">Montas:</label>
-                    <input className="form-control col-sm-6 " placeholder="Montas"/>
-                    <label className="text-black col-sm-6 ">Partos y #crias:</label>
-                    <input className="form-control col-sm-6 " placeholder="Partos y crias"/>
-                    <label className="text-black col-sm-6 ">Hallazgos anormales:</label>
-                    <input className="form-control col-sm-6 " placeholder="Hallazgos anormales"/>
+                    <strong className="text-black col-sm-6 col-md-5  strong">Sistema Reproductivo</strong>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="status"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Vulva/Pene:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Vulva/pene"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Escroto y testiculos:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Escroto y testiculos"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Ultimo celo:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Ultimo celo"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Montas:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Montas"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Partos y #crias:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Partos y crias"/>
+                    <label className="text-black col-sm-6 col-md-5 ">Hallazgos anormales:</label>
+                    <input className="form-control col-sm-6 col-md-7 " placeholder="Hallazgos anormales"/>
                 </div>
 
             </div>
